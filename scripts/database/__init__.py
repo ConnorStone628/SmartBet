ba2615fe209db41797196c6f53774f963abeb52f
Connor Stone <connor_mc2@hotmail.com>
2016-03-03 14:24:34 -0500

    More readme's

--- scripts/database/__init__.py
+++ scripts/database/__init__.py
@@ -3,3 +3,9 @@
 # Authors: Connor Stone
 # Date: 02-03-2016
 ################################################################################
+
+
+class Database:
+
+    def __init__(self, readonly = True):
+        self.readonly = readonly
