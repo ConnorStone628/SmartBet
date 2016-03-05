c001860712e17732e4a009d6924d531c30fdac84
Connor Stone <connor_mc2@hotmail.com>
2016-03-04 22:57:34 -0500

    Removed shared directory as it was not accessible. Python does not support a simple method to traverse back up the directory tree.

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -4,6 +4,9 @@
 # Date: 02-03-2016
 ################################################################################
 
+def testing():
+    print 'i am scripts'
+
 #import scoring
 #import lineup
 #import database
